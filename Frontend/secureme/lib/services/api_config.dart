3a8a9564e9efe56ed5f0183f84fd0c83bdf07034
Soham1250 <154613808+Soham1250@users.noreply.github.com>
2025-01-03 23:04:26 +0530

    For testing purposes of android

--- Frontend/secureme/lib/services/api_config.dart
+++ Frontend/secureme/lib/services/api_config.dart
@@ -1,10 +1,10 @@
 class ApiConfig {
   // Using HTTPS for secure connection
   //for deployed version
-  // static const String baseUrl = 'https://secureme-vj4u.onrender.com';
+  static const String baseUrl = 'https://secureme-vj4u.onrender.com';
 
   //for testing version
-  static const String baseUrl = 'http://localhost:4000';
+  // static const String baseUrl = 'http://localhost:4000';
 
   // API endpoints
   static const String scanUrl = '$baseUrl/api/url/scan';
