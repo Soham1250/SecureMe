5fec303fb530038a4234d54b49cfab9f4f9d12e2
Soham1250 <154613808+Soham1250@users.noreply.github.com>
2025-01-03 19:31:58 +0530

    changes in base url

--- Frontend/secureme/lib/services/api_config.dart
+++ Frontend/secureme/lib/services/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // Replace this with your Render URL once deployed
-  static const String baseUrl = 'https://secureme-backend.onrender.com';
-  
+  static const String baseUrl = 'https://secureme-vj4u.onrender.com';
+
   // API endpoints
   static const String scanUrl = '$baseUrl/api/url/scan';
   static const String healthCheck = '$baseUrl/health';
