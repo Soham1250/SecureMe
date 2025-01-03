5019ca03548d238ac28c2b4a14f6db7adbe07141
Soham1250 <154613808+Soham1250@users.noreply.github.com>
2025-01-03 22:50:38 +0530

    classification of baseUrl

--- Frontend/secureme/lib/services/api_config.dart
+++ Frontend/secureme/lib/services/api_config.dart
@@ -1,6 +1,9 @@
 class ApiConfig {
   // Using HTTPS for secure connection
+  //for deployed version
   // static const String baseUrl = 'https://secureme-vj4u.onrender.com';
+
+  //for testing version
   static const String baseUrl = 'http://localhost:4000';
 
   // API endpoints
