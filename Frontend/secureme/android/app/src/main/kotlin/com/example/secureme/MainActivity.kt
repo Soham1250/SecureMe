94a49805e11856c49ccf8166050a98d273001179
Soham1250 <soham.pansare1250@gmail.com>
2025-08-22 23:12:23 +0530

    Flutter Fragement Activity Implement for enhanced biometrics

--- Frontend/secureme/android/app/src/main/kotlin/com/example/secureme/MainActivity.kt
+++ Frontend/secureme/android/app/src/main/kotlin/com/example/secureme/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.secureme
 
 import android.content.Intent
 import android.provider.Settings
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS
 
-class MainActivity: FlutterActivity() {
-    private val CHANNEL = "com.example.secureme/link_analysis"
+class MainActivity: FlutterFragmentActivity() {
+    private val CHANNEL = "com.example.secureme/link_anIalysis"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
